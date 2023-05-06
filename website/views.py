67c32c8ea5900b721259570a48e9658e5478bb8f
candace lebby <candace.lebby@outlook.com>
2023-05-06 21:48:41 +0100

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages 
 
-# Create your views here.
+def home (request):
+    return render(request, 'home.html', {})
+
+#def login_user request:
+    #return(render, request, "authentication/login.html") 
+
+#check if user is logging in 
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful!")
+            return redirect('home')
+        else:
+            messages.success(request, "Whoops, There Was A Problem Logging You In, Please Try Again..")
+            return redirect('login')
+    else: 
+        return render(request, 'login.html', {}) 
+    
+
+def logout_user (request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+
+#Request is sent to backend here, we pass it nito the view and return the homepage in the first example. 
