2c5fb33ac078bdb6928ed41e71cc2d56310aa538
candace lebby <candace.lebby@outlook.com>
2023-05-07 18:53:55 +0100

    Removed login page

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    created_at  = models.DateTimeField(auto_now_add=True) #Anytime a new customer record is created Django puts a time stamp on that
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=75)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    postcode =  models.CharField(max_length=10)
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
--- website/views.py
+++ website/views.py
@@ -1,39 +1,50 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages 
+from .forms  import SignUpForm
+from .models import Customer
 
-def home (request):
-    return render(request, 'home.html', {})
 
-#def login_user request:
-    #return(render, request, "authentication/login.html") 
 
-#check if user is logging in 
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        #Authenticate 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Login Successful!")
-            return redirect('home')
-        else:
-            messages.success(request, "Whoops, There Was A Problem Logging You In, Please Try Again..")
-            return redirect('login')
-    else: 
-        return render(request, 'login.html', {}) 
-    
-
+def home(request):
+        customers = Customer.objects.all()
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+             #Authenticate 
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Login Successful!")
+                return render(request,'home.html')
+            else:
+                messages.success(request, "Whoops, There Was A Problem Logging You In, Please Try Again..")
+                return ('home')
+        else: 
+            return render(request, 'home.html', {'customers':Customer}) 
+        
 def logout_user (request):
     logout(request)
     messages.success(request, "You Have Been Logged Out")
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
-
+        if request.method == 'POST':
+                form = SignUpForm(request.POST)
+                if form.is_valid():
+                        form.save()
+                #Authenticate and login
+                username= form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                messages.success(request, "You Have Successfully Registered! Welcome!")
+                return redirect('home')
+        else:
+                form=SignUpForm()
+                return render(request, 'register.html', {'form':form})
+        
 
 #Request is sent to backend here, we pass it nito the view and return the homepage in the first example. 
+
+
