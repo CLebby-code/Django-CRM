8976f75a928f28ad42bac2cc78763f2e270c0ebf
Candace Lebby <clebby@zaizi.com>
2023-06-21 20:42:14 +0100

    black/flake8 updates

--- website/urls.py
+++ website/urls.py
@@ -12,9 +12,7 @@ urlpatterns = [
     path("delete_record/<int:pk>", views.delete_record, name="delete_record"),
     path("add_customer/", views.add_customer, name="add_customer"),
     path("add_company/", views.add_company, name="add_company"),
-    path(
-        "update_customer/<int:pk>", views.update_customer, name="update_customer"
-    ),  # noqa: E501
+    path("update_customer/<int:pk>", views.update_customer, name="update_customer"),  # noqa: E501
     path("company_info/<int:pk>", views.company_info, name="company_info"),
     path("update_company/", views.update_company, name="update_company"),
     path("delete_company/", views.delete_company, name="delete_company"),
