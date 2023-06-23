b863a2996cba7b9c3faaf2e69cd48d3984e022a7
Candace Lebby <clebby@zaizi.com>
2023-06-23 10:20:29 +0100

    flake 8

--- website/forms.py
+++ website/forms.py
@@ -51,9 +51,7 @@ class SignUpForm(UserCreationForm):
         ].help_text = "<ul class=\"form-text text-muted small\"><li>Your password can't be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can't be a commonly used password.</li><li>Your password can't be entirely numeric.</li></ul>"  # noqa: E501
 
         self.fields["password2"].widget.attrs["class"] = "form-control"
-        self.fields["password2"].widget.attrs[
-            "placeholder"
-        ] = "Confirm Password"  
+        self.fields["password2"].widget.attrs["placeholder"] = "Confirm Password"
         self.fields["password2"]
 
 
