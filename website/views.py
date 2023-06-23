e058db0b586a72387eb60e83f178a9a2423d5b56
JEREMY LORD <jlord@zaizi.com>
2023-06-23 08:30:26 +0000

    fix views.py imports

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm, AddCustomerForm, AddCompanyForm
 from .models import Customer, Company
