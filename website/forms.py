a9ce211431d4cb3d7ac8779c322b225e29c68650
Candace Lebby <clebby@zaizi.com>
2023-06-26 11:09:30 +0100

    this is not finished

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Customer, Company, Notes
+from .models import Customer, Company, Notes 
 
 
 class SignUpForm(UserCreationForm):
