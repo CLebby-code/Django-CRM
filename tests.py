736e79a5f8e0a1bcb47be9cfae97e66effa94ea9
JEREMY LORD <jlord@zaizi.com>
2023-07-04 09:11:18 +0000

    customer deets test

--- tests.py
+++ tests.py
@@ -20,11 +20,20 @@ class ViewsTestCase(TestCase):
     def setUp(self):
         comp = Company.objects.create(name="company 1")
         Customer.objects.create(first_name="bob", last_name="dobalina", company=comp)
+        user = User.objects.create_user(username="testuser", password="12345")
 
     def test_customer_list(self):
         c = Client()
-        user = User.objects.create_user(username="testuser", password="12345")
         login = c.login(username="testuser", password="12345")
         response = c.get("/")
         self.assertIn("<table", str(response.content))
         self.assertIn("bob dobalina", str(response.content))
+
+    def test_customer_details(self):
+        c = Client()
+        login = c.login(username="testuser", password="12345")
+        cust = Customer.objects.get(first_name="bob", last_name="dobalina")
+        response = c.get("/record/%s" % cust.id)
+        self.assertIn("bob dobalina", str(response.content))
+        self.assertIn("Email", str(response.content))
+        self.assertIn("Phone", str(response.content))
